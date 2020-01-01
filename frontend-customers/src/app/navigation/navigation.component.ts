10e76d7457db06462b99d32950e8b0ed00984820
Niklas Studer <niklas.studer@diva-e.com>
2020-01-01 22:19:41 +0100

    add main Navigation

---
+++ frontend-customers/src/app/navigation/navigation.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.XSmall)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+  loggedIn = true;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  login() {
+    this.loggedIn = !this.loggedIn;
+  }
+
+  openMyAccount() {
+
+  }
+
+}
