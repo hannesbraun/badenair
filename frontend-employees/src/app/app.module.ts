d96678b8a935ab9bf2558528647ca41611cc5e28
Jan Stürzel <janstuerzel021@gmail.com>
2020-01-12 16:14:57 +0100

    Add conflict dialog

--- frontend-employees/src/app/app.module.ts
+++ frontend-employees/src/app/app.module.ts
@@ -6,21 +6,25 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlightInfoDialogComponent} from './components/flight-overview/dialogs/flight-info-dialog/flight-info-dialog.component';
 import {FlightOverviewComponent} from './components/flight-overview/flight-overview.component';
 import {MaterialModule} from './material.module';
+import {ScheduleConflictDialogComponent} from './components/flight-overview/dialogs/schedule-conflict-dialog/schedule-conflict-dialog.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [
         AppComponent,
         FlightInfoDialogComponent,
-        FlightOverviewComponent
+        FlightOverviewComponent,
+        ScheduleConflictDialogComponent
     ],
     imports: [
         BrowserAnimationsModule,
         BrowserModule,
         MaterialModule,
+        ReactiveFormsModule
     ],
     providers: [],
     bootstrap: [AppComponent],
-    entryComponents: [FlightInfoDialogComponent]
+    entryComponents: [FlightInfoDialogComponent, ScheduleConflictDialogComponent]
 })
 export class AppModule {
 }
