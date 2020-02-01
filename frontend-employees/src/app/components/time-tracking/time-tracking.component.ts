1e9164bdfd9996639c5fc0751615a4c25b56a48e
Niklas Studer <niklas.studer@diva-e.com>
2020-02-01 17:36:50 +0100

    add start/stop function

--- frontend-employees/src/app/components/time-tracking/time-tracking.component.ts
+++ frontend-employees/src/app/components/time-tracking/time-tracking.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-time-tracking',
-  templateUrl: './time-tracking.component.html',
-  styleUrls: ['./time-tracking.component.scss']
+    selector: 'app-time-tracking',
+    templateUrl: './time-tracking.component.html',
+    styleUrls: ['./time-tracking.component.scss']
 })
 export class TimeTrackingComponent implements OnInit {
+    elapsedTime: number = 0;
+    timer: any;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    start() {
+        this.timer = setInterval(this.updateTime, 1000);
+    }
+
+    stop() {
+        clearInterval(this.timer);
+    }
+
+    reset() {
+        this.elapsedTime = 0;
+    }
+
+    updateTime = () => {
+        this.elapsedTime++;
+    }
 }
