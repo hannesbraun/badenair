ef033b294115df4b6261f5ceaa1b1b23629a14eb
Jan Stürzel <janstuerzel021@gmail.com>
2020-02-29 14:48:47 +0100

    Update angular material in customer frontend

--- frontend-customers/src/app/components/profile/profile.component.ts
+++ frontend-customers/src/app/components/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {SignUpDto, UpdateProfileDto} from '../../services/dtos/Dtos';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {MatDialog} from '@angular/material';
+import {MatDialog} from '@angular/material/dialog';
 import {
     ChangePasswordDialogComponent,
     ChangePasswordDialogOutput
