04096e4bbbc1a75ff546e265c6b7872b6eade828
Niklas Studer <niklas.studer@diva-e.com>
2020-03-05 18:53:28 +0100

    add Calender Highlighting

--- frontend-employees/src/app/components/vacation-planning/vacation-planning.component.ts
+++ frontend-employees/src/app/components/vacation-planning/vacation-planning.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {MatCalendarCellCssClasses} from '@angular/material/datepicker';
 
 enum vacationState {
     pending = 'Ausstehend',
@@ -12,6 +13,10 @@ enum vacationState {
 })
 export class VacationPlanningComponent implements OnInit {
 
+    calendar1: Date;
+    calender2: Date;
+    calender3: Date;
+
     dataSource = [
         {duration: [new Date(), new Date()], days: 3, state: vacationState.pending},
         {duration: [new Date(), new Date()], days: 4, state: vacationState.approved},
@@ -20,10 +25,44 @@ export class VacationPlanningComponent implements OnInit {
 
     displayedColumns: string[] = ['duration', 'days', 'state'];
 
+    approvedDates = [
+        [2, 3, 4, 5, 6, 7],
+        [7, 8, 9, 10, 11, 12, 13],
+        [13, 14],
+        [23, 24, 25]
+    ];
+
+    pendingDates = [
+        [1],
+        [24, 25],
+        [],
+        [12, 13, 14, 15, 16]
+    ];
+
     constructor() {
     }
 
     ngOnInit(): void {
+        const currentDate = new Date();
+        this.calendar1 = new Date(currentDate.getFullYear(), (currentDate.getMonth() + 1) % 11);
+        this.calendar2 = new Date(currentDate.getFullYear(), (currentDate.getMonth() + 2) % 11);
+        this.calendar3 = new Date(currentDate.getFullYear(), (currentDate.getMonth() + 3) % 11);
     }
 
+    getSelectedDates(calenderNumber: number) {
+        const approvedDate = this.approvedDates[calenderNumber];
+        const pendingDate = this.pendingDates[calenderNumber];
+
+        return (d: Date): MatCalendarCellCssClasses => {
+            const date = d.getDate();
+
+            if (approvedDate.includes(date)) {
+                return 'approved-date';
+            }
+            if (pendingDate.includes(date)) {
+                return 'pending-date';
+            }
+            return '';
+        };
+    }
 }
