9cf05a8b4704fe8db769e8063d22a9b2f6c39134
Hannes Braun <hannesbraun@mail.de>
2020-03-14 20:51:38 +0100

    Various fixes for success page

--- frontend-customers/src/app/pages/success-page/success-page.component.ts
+++ frontend-customers/src/app/pages/success-page/success-page.component.ts
@@ -1,39 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+
+export interface Flight {
+    start: string;
+    destination: string;
+    departureDate: Date;
+    arrivalDate: Date;
+}
 
 @Component({
     selector: 'app-success-page',
     templateUrl: './success-page.component.html',
     styleUrls: ['./success-page.component.scss']
 })
-export class SuccessPageComponent implements OnInit {
+export class SuccessPageComponent {
 
-    start: string | undefined;
-    destination: string | undefined;
-    departureDate1: Date | undefined;
-    arrivalDate1: Date | undefined;
-    departureDate2: Date | undefined;
-    arrivalDate2: Date | undefined;
+    flights: Flight[] | undefined;
 
     constructor() {
+        this.flights = [{
+            start: "Basel",
+            destination: "Porto",
+            departureDate: new Date(),
+            arrivalDate: new Date()
+        }, {
+            start: "Porto",
+            destination: "Basel",
+            departureDate: new Date(),
+            arrivalDate: new Date()
+        }]
     }
 
-    ngOnInit(): void {
-        this.start = "Basel";
-        this.destination = "Porto";
-
-        this.departureDate1 = new Date();
-        this.arrivalDate1 = new Date();
-        this.departureDate2 = new Date();
-        this.arrivalDate2 = new Date();
-    }
-
-    getDuration(id: number): number {
-        if (id === 1 && this.departureDate1 && this.arrivalDate1) {
-            return this.arrivalDate1.getTime() - this.departureDate1.getTime();
-        } else if (id === 2 && this.departureDate2 && this.arrivalDate2) {
-            return this.arrivalDate2.getTime() - this.departureDate2.getTime();
-        } else {
-            return 0;
-        }
+    getDuration(flight: Flight): number {
+        return flight.arrivalDate.getTime() - flight.departureDate.getTime();
     } 
 }
