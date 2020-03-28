92348010ec6ec83681f69e000a9fb958721a8e09
Jan Stürzel <janstuerzel021@gmail.com>
2020-03-28 15:24:44 +0100

    Add values to enums

--- server/src/main/java/de/hso/badenair/domain/booking/LuggageState.java
+++ server/src/main/java/de/hso/badenair/domain/booking/LuggageState.java
@@ -1,5 +1,13 @@
 package de.hso.badenair.domain.booking;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
 public enum LuggageState {
-    // TODO: Add Luggage states
+    ON_PLANE("Im Flugzeug"),
+    READY_FOR_PICK_UP("Abholbereit");
+
+    @Getter
+    private String name;
 }
