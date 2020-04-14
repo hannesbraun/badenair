7a27902966f83673e45fe9e9a7d8e735bfb53c3c
Hannes Braun <hannesbraun@mail.de>
2020-04-14 18:44:19 +0200

    Fix backend url for employees

--- frontend-employees/src/environments/environment.prod.ts
+++ frontend-employees/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    backendApiRoot: 'http://192.52.35.130:8081/api/customer',
+    backendApiRoot: 'http://192.52.35.130:8081/api/employees',
     authUrl: 'http://192.52.35.130:8080/auth/realms/badenair',
 };
