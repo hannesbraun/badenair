f68045cddb4bfc2b88e760be0b962071110992ad
Hannes Braun <hannesbraun@mail.de>
2020-04-24 17:57:19 +0200

    Fix TravelerRepository

--- server/src/main/java/de/hso/badenair/service/traveler/TravelerRepository.java
+++ server/src/main/java/de/hso/badenair/service/traveler/TravelerRepository.java
@@ -1,8 +1,10 @@
 package de.hso.badenair.service.traveler;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import de.hso.badenair.domain.booking.Traveler;
 
+@Repository
 public interface TravelerRepository extends CrudRepository<Traveler, Long> {
 }
