14a814dab3489c69d772f0c3362b5369f4d2306d
Friedrich Schmidt <fschmid3@stud.hs-offenburg.de>
2020-04-27 17:08:51 +0200

    small changes on Controller

--- server/src/main/java/de/hso/badenair/controller/flight/FlightController.java
+++ server/src/main/java/de/hso/badenair/controller/flight/FlightController.java
@@ -16,14 +16,12 @@ public class FlightController {
     private final FlightService flightService;
 
     @PatchMapping("/tracking/{id}")
-    public ResponseEntity<?>  updateFlightTracking (@PathVariable long id, @RequestBody String action){
+    public ResponseEntity<Object>  updateFlightTracking (@PathVariable Long id, @RequestBody String action){
         boolean updateSuccess = flightService.updateFlightTracking(id, action);
 
         if (updateSuccess) {
             return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
         } else {
-            // The only possible failure is probably that the luggage id isn't
-            // present in the database
             return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
         }
     }
