ee0665a64ee71d829045dc0719fdcdc25eb5580f
Niklas Studer <niklas.studer@diva-e.com>
2020-05-01 11:37:06 +0200

    add Seat formArray

--- frontend-customers/src/app/components/seat-selection/seat-selection.component.ts
+++ frontend-customers/src/app/components/seat-selection/seat-selection.component.ts
@@ -1,12 +1,12 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AbstractControl, FormArray, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-seat-selection',
     templateUrl: './seat-selection.component.html',
     styleUrls: ['./seat-selection.component.scss']
 })
-export class SeatSelectionComponent {
+export class SeatSelectionComponent implements OnInit{
     columns = ['A', 'B', 'C', 'D', 'E', 'F'];
     rows: number[] = [];
 
@@ -23,10 +23,38 @@ export class SeatSelectionComponent {
         }
     }
 
+    ngOnInit(): void {
+        this.seatForm = this.formBuilder.group({
+            items: this.formBuilder.array([])
+        });
+        this.createFormArray(3);
+    }
+
     submit() {
         if (this.seatForm.invalid) {
             return;
         }
         this.seatSelected.emit(this.seatForm.value);
+    }
+
+    get formArray(): FormArray {
+        return (this.seatForm.controls.items as FormArray);
+    }
+
+    get forms(): AbstractControl[] {
+        return this.formArray.controls;
+    }
+
+    createFormArray(count: number) {
+        for (let i = 0; i < count; i++) {
+            this.appendSeat();
+        }
+    }
+
+    appendSeat() {
+        this.formArray.push(this.formBuilder.group({
+            row: ['', Validators.required],
+            column: ['', Validators.required],
+        }));
     }
 }
