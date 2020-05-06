b40ec4511814348f432ebf551d7a38bbce9490cc
Friedrich Schmidt <fschmid3@stud.hs-offenburg.de>
2020-05-06 18:30:24 +0200

    delete FlightControllerTest

--- server/src/main/java/de/hso/badenair/controller/flight/FlightController.java
+++ server/src/main/java/de/hso/badenair/controller/flight/FlightController.java
@@ -2,7 +2,6 @@ package de.hso.badenair.controller.flight;
 
 
 import de.hso.badenair.service.flight.FlightService;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
