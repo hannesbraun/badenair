dd0436db1f8398527509d1db93cef3ce59f28062
Tenetar <wariorcat@web.de>
2020-05-09 21:27:40 +0200

    .12: Auto stash before rebase of "refs/heads/feature/BA-15.12"

--- server/src/main/java/de/hso/badenair/controller/flightplan/FlightplanController.java
+++ server/src/main/java/de/hso/badenair/controller/flightplan/FlightplanController.java
@@ -11,28 +11,16 @@ import java.util.ArrayList;
 
 import java.time.OffsetDateTime;
 
+@RestController
+@RequestMapping("/api/employee/flightplan")
 @RequiredArgsConstructor
 public class FlightplanController {
     private final FlightplanService flightplanService;
 
     @GetMapping
-<<<<<<< Updated upstream:server/src/main/java/de/hso/badenair/controller/flightplan/FlightController.java
-    public ResponseEntity<PlaneScheduleDto[]> getPlaneSchedules() {
-<<<<<<< Updated upstream:server/src/main/java/de/hso/badenair/controller/flightplan/FlightplanController.java
-        return null;
-=======
-        FlightDto temp1 = new FlightDto();
-        PlaneScheduleDto temp = new PlaneScheduleDto();
-    
-        PlaneScheduleDto test[];
-
-        return ResponseEntity.ok(test);
-=======
     public ResponseEntity<ArrayList<PlaneScheduleDto>> getPlaneSchedules() {
 
 
         return ResponseEntity.ok(flightplanService.getPlaneSchedules());
->>>>>>> Stashed changes:server/src/main/java/de/hso/badenair/controller/flightplan/FlightplanController.java
->>>>>>> Stashed changes:server/src/main/java/de/hso/badenair/controller/flightplan/FlightController.java
     }
 }
