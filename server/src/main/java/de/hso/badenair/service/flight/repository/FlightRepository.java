3b576087a854af0d739b8f6892fc2811f13b6685
Janessus <janesheuberger@web.de>
2020-05-26 15:52:26 +0200

    statistic service

--- server/src/main/java/de/hso/badenair/service/flight/repository/FlightRepository.java
+++ server/src/main/java/de/hso/badenair/service/flight/repository/FlightRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FlightRepository extends CrudRepository<Flight, Long> {
+    List<Flight> findByStartDateAfter(OffsetDateTime date);
 }
--- server/src/main/java/de/hso/badenair/service/plane/repository/PlaneRepository.java
+++ server/src/main/java/de/hso/badenair/service/plane/repository/PlaneRepository.java
@@ -1,10 +1,16 @@
 package de.hso.badenair.service.plane.repository;
 
+import de.hso.badenair.domain.flight.Flight;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import de.hso.badenair.domain.plane.Plane;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 @Repository
 public interface PlaneRepository extends CrudRepository<Plane, Long> {
+    List<Flight> findByStartDateAfter(OffsetDateTime date);
+
 }
