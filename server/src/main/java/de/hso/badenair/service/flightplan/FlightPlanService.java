31044f78843e52a16679d2ad186f5bebac4bb7b1
Hannes Braun <hannesbraun@mail.de>
2020-06-06 18:31:47 +0200

    Fix flightplan timezone issue

--- server/src/main/java/de/hso/badenair/service/flightplan/FlightPlanService.java
+++ server/src/main/java/de/hso/badenair/service/flightplan/FlightPlanService.java
@@ -1,5 +1,17 @@
 package de.hso.badenair.service.flightplan;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
 import de.hso.badenair.controller.dto.flightplan.ConflictDto;
 import de.hso.badenair.controller.dto.flightplan.FlightWithoutPriceDto;
 import de.hso.badenair.controller.dto.flightplan.PlaneScheduleDto;
@@ -8,84 +20,73 @@ import de.hso.badenair.domain.plane.Plane;
 import de.hso.badenair.service.plane.repository.PlaneRepository;
 import de.hso.badenair.util.time.DateFusioner;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FlightPlanService {
 
-    private final FlightplanRepository flightplanRepository;
-    private final PlaneRepository planeRepository;
-    private ArrayList<ConflictDto> conflicts = new ArrayList<>();
+	private final FlightplanRepository flightplanRepository;
+	private final PlaneRepository planeRepository;
+	private ArrayList<ConflictDto> conflicts = new ArrayList<>();
 
-    public ArrayList<PlaneScheduleDto> getPlaneSchedules() {
-        OffsetDateTime now = OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.of("+1"));;
-        now = now.truncatedTo(ChronoUnit.DAYS);
-        now = now.plusHours(6);
-        OffsetDateTime oneDayLater = now.plusHours(21);
-        AtomicBoolean hasConflict = new AtomicBoolean(false);
+	public ArrayList<PlaneScheduleDto> getPlaneSchedules() {
+		OffsetDateTime now = OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.of("+1"));
+		now = now.truncatedTo(ChronoUnit.DAYS);
+		now = now.plusHours(6);
+		OffsetDateTime oneDayLater = now.plusHours(21);
+		AtomicBoolean hasConflict = new AtomicBoolean(false);
 
-        List<Plane> planes = (List<Plane>) planeRepository.findAll();
-        ArrayList<PlaneScheduleDto> planeSchedules = new ArrayList<>(0);
+		List<Plane> planes = (List<Plane>) planeRepository.findAll();
+		ArrayList<PlaneScheduleDto> planeSchedules = new ArrayList<>(0);
 
-        OffsetDateTime finalNow = now;
-        planes.forEach(plane -> {
-            hasConflict.set(false);
+		OffsetDateTime finalNow = now;
+		planes.forEach(plane -> {
+			hasConflict.set(false);
 
-            final List<FlightWithoutPriceDto> flights = flightplanRepository
-                .findByStartDateBetweenAndPlane_IdEquals(finalNow.withHour(0).withMinute(0).withSecond(0),
-                    oneDayLater.withHour(23).withMinute(59).withSecond(59), plane.getId())
-                .stream().filter(flight -> {
-                    OffsetDateTime fusionedStartDate = DateFusioner.fusionStartDate(flight.getStartDate(),
-                        flight.getScheduledFlight().getStartTime(), null);
-                    return fusionedStartDate.isAfter(finalNow) && fusionedStartDate.isBefore(oneDayLater);
-                })
+			final List<FlightWithoutPriceDto> flights = flightplanRepository
+					.findByStartDateBetweenAndPlane_IdEquals(finalNow.withHour(0).withMinute(0).withSecond(0),
+							oneDayLater.withHour(23).withMinute(59).withSecond(59), plane.getId())
+					.stream().filter(flight -> {
+						OffsetDateTime fusionedStartDate = DateFusioner.fusionStartDate(flight.getStartDate(),
+								flight.getScheduledFlight().getStartTime(), null);
+						return fusionedStartDate.isAfter(finalNow) && fusionedStartDate.isBefore(oneDayLater);
+					})
 
+					.map(flight -> {
+						final ScheduledFlight scheduledFlight = flight.getScheduledFlight();
 
+						for (int i = 0; i < conflicts.size(); i++) {
+							if (flight.getId() == conflicts.get(i).getFlightID())
+								hasConflict.set(true);
+						}
 
-                .map(flight -> {
-                    final ScheduledFlight scheduledFlight = flight.getScheduledFlight();
+						// Adjusting timezones in these dtos again because the frontend wants another
+						// timezone :(
+						return new FlightWithoutPriceDto(flight.getId(), scheduledFlight.getStartingAirport().getName(),
+								scheduledFlight.getDestinationAirport().getName(),
+								DateFusioner
+										.fusionStartDate(flight.getStartDate(), scheduledFlight.getStartTime(), null)
+										.withOffsetSameLocal(ZoneOffset
+												.of(TimeZone.getDefault().inDaylightTime(new Date()) ? "+2" : "+1")),
+								DateFusioner
+										.fusionArrivalDate(flight.getStartDate(), scheduledFlight.getStartTime(),
+												scheduledFlight.getDurationInHours(), null)
+										.withOffsetSameLocal(ZoneOffset
+												.of(TimeZone.getDefault().inDaylightTime(new Date()) ? "+2" : "+1")),
+								flight.getActualStartTime(), flight.getActualLandingTime());
+					}).collect(Collectors.toList());
 
-                    for (int i = 0; i < conflicts.size(); i++)
-                    {
-                        if (flight.getId() == conflicts.get(i).getFlightID())
-                            hasConflict.set(true);
-                    }
+			planeSchedules.add(new PlaneScheduleDto(plane.getId(), plane.getTypeData().getType().getName(),
+					plane.getState().getName(), hasConflict.get(), flights));
+		});
 
-                    return new FlightWithoutPriceDto(flight.getId(),
-                        scheduledFlight.getStartingAirport().getName(),
-                        scheduledFlight.getDestinationAirport().getName(),
-                        DateFusioner.fusionStartDate(flight.getStartDate(), scheduledFlight.getStartTime(), null),
-                        DateFusioner.fusionArrivalDate(flight.getStartDate(), scheduledFlight.getStartTime(), scheduledFlight.getDurationInHours(), null),
-                        flight.getActualStartTime(),
-                        flight.getActualLandingTime());
-                })
-                .collect(Collectors.toList());
+		conflicts.clear();
+		ConflictFinder.findConflicts(planeSchedules, conflicts);
 
+		return planeSchedules;
+	}
 
-
-            planeSchedules.add(new PlaneScheduleDto(plane.getId(),
-                plane.getTypeData().getType().getName(),
-                plane.getState().getName(),
-                hasConflict.get(),
-                flights));
-        });
-
-        conflicts.clear();
-        ConflictFinder.findConflicts(planeSchedules, conflicts);
-
-        return planeSchedules;
-    }
-
-    public ArrayList<ConflictDto> getConflicts() {
-        return conflicts;
-    }
+	public ArrayList<ConflictDto> getConflicts() {
+		return conflicts;
+	}
 }
