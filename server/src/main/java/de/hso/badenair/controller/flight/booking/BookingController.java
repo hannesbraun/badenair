0e5bc81f4a4c7f336a0daf136a68c0daac2ed720
Hannes Braun <hannesbraun@mail.de>
2020-06-06 23:46:00 +0200

    Fix annoying booking bugs
    For more information: read the corresponding pull request :)

--- server/src/main/java/de/hso/badenair/controller/flight/booking/BookingController.java
+++ server/src/main/java/de/hso/badenair/controller/flight/booking/BookingController.java
@@ -1,8 +1,9 @@
 package de.hso.badenair.controller.flight.booking;
 
-import de.hso.badenair.controller.dto.flight.IncomingBookingDto;
-import de.hso.badenair.service.flight.booking.BookingService;
-import lombok.RequiredArgsConstructor;
+import java.security.Principal;
+
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,26 +11,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.security.Principal;
+import de.hso.badenair.controller.dto.flight.IncomingBookingDto;
+import de.hso.badenair.service.flight.booking.BookingService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/customer/flight")
 @RequiredArgsConstructor
 public class BookingController {
-    private final BookingService bookingService;
+	private final BookingService bookingService;
 
-    @PostMapping("/booking")
-    public ResponseEntity<?> bookFlight(Principal user, @RequestBody @Valid IncomingBookingDto dto){
-        boolean success = bookingService.bookFlight(user.getName(), dto);
+	@PostMapping("/booking")
+	public ResponseEntity<Integer> bookFlight(Principal user, @RequestBody @Valid IncomingBookingDto dto) {
+		int id = bookingService.bookFlight(user.getName(), dto);
 
-        if(success){
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-        }
-        else{
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+		if (id != 0) {
+			return ResponseEntity.ok(id);
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 
-    }
+	}
+
+	@PostMapping("/confirmBooking")
+	public ResponseEntity<?> confirmBooking(Principal user, @RequestBody @Valid Integer[] bookingIds) {
+		boolean success = bookingService.finishBooking(user.getName(), bookingIds);
+
+		if (success) {
+			return ResponseEntity.status(HttpStatus.CREATED).build();
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+
+	}
 
 }
