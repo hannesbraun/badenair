6e4eda412130b025de494426bccae3282f08ce8d
Janessus <janesheuberger@web.de>
2020-06-15 09:59:29 +0200

    statistic service WIP

--- server/src/main/java/de/hso/badenair/service/statistics/Statistic.java
+++ server/src/main/java/de/hso/badenair/service/statistics/Statistic.java
@@ -1,5 +1,15 @@
 package de.hso.badenair.service.statistics;
 
+import de.hso.badenair.domain.booking.Booking;
+import de.hso.badenair.domain.flight.Flight;
+import de.hso.badenair.domain.plane.PlaneType;
+import de.hso.badenair.service.flight.repository.FlightRepository;
+import de.hso.badenair.util.time.DateFusioner;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -7,33 +17,30 @@ import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Set;
 
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import de.hso.badenair.domain.booking.Booking;
-import de.hso.badenair.domain.flight.Flight;
-import de.hso.badenair.domain.plane.PlaneType;
-import de.hso.badenair.service.flight.repository.FlightRepository;
-import lombok.RequiredArgsConstructor;
-
 @Service
 @RequiredArgsConstructor
 public class Statistic {
-	private final FlightRepository flightRepository;
+    private final FlightRepository flightRepository;
 
-	// @Scheduled (fixedRate = 86_400_000)
-	@Scheduled(fixedRate = 10_000)
-	@Transactional
-	public void saveStatistic() {
-		OffsetDateTime startDate = OffsetDateTime.now().withSecond(0).withMinute(0).withHour(0);
+    //@Scheduled (fixedRate = 86_400_000)
+    @Scheduled (fixedRate = 10_000)
+    @Transactional
+    public void saveStatistic()
+    {
+        System.out.println("Generating Statistics");
+        OffsetDateTime startDate = OffsetDateTime.now().withSecond(0).withMinute(0).withHour(0);
 
 		List<Flight> flights = flightRepository.findByStartDateAfter(startDate);
 
-		for (int i = 0; i < flights.size(); i++) {
-			OffsetDateTime startTime = flights.get(i).getActualStartTime();
-			OffsetDateTime landingTime = flights.get(i).getActualLandingTime();
-			Long id = flights.get(i).getId();
+        for(int i = 0; i < flights.size(); i++) {
+            OffsetDateTime startTime = DateFusioner.fusionStartDate(flights.get(i).getStartDate(),
+                flights.get(i).getScheduledFlight().getStartTime(),
+                null);
+            OffsetDateTime landingTime = DateFusioner.fusionArrivalDate(flights.get(i).getStartDate(),
+                flights.get(i).getScheduledFlight().getStartTime(),
+                flights.get(i).getScheduledFlight().getDurationInHours(),
+                null);
+            Long id = flights.get(i).getId();
 
 			double sales = 0;
 			Set<Booking> bookings = flights.get(i).getBookings();
@@ -47,8 +54,13 @@ public class Statistic {
             try {
                 FileWriter fileWriter = new FileWriter("statistics.txt", false);
                 PrintWriter printWriter = new PrintWriter(fileWriter);
-                printWriter.printf("Statistics for %s\n\nPlaneID:\t\t%d\nPlaneType:\t\t%s\nStartTime:\t\t%s\nLandingTime:\t\t%s\nSales:\t\t%f\n",
-                    OffsetDateTime.now().toString(), id, planeType.toString(), startDate.toString(), landingTime.toString(), sales);
+                printWriter.printf("Statistics for %s\n\nPlaneID:\t\t%d\nPlaneType:\t\t%s\nStartTime:\t\t%s\nLandingTime:\t%s\nSales:\t\t\t%f\n",
+                    OffsetDateTime.now().toString(),
+                    id,
+                    planeType.toString(),
+                    startTime.toString(),
+                    landingTime.toString(),
+                    sales);
 
 				fileWriter.flush();
 				fileWriter.close();
