70cc2acfb2969e66399e4583c324fb38f173175c
Niklas Studer <niklas.studer@diva-e.com>
2020-06-28 23:37:45 +0200

    change Environment files

--- frontend-customers/src/environments/environment.prod.ts
+++ frontend-customers/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    backendApiRoot: 'http://192.52.35.130:8081/api/customer',
-    authUrl: 'http://192.52.35.130:8080/auth/realms/badenair',
+    backendApiRoot: 'http://pils.gq:42069/api/customer',
+    authUrl: 'http://pils.gq:8180/auth/realms/badenair',
     home: 'http://www.badenair-customers.lovely-golden.de:80'
 };
--- frontend-employees/src/environments/environment.prod.ts
+++ frontend-employees/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    backendApiRoot: 'http://192.52.35.130:8081/api/employee',
-    authUrl: 'http://192.52.35.130:8080/auth/realms/badenair',
+    backendApiRoot: 'http://pils.gq:42069/api/employee',
+    authUrl: 'http://pils.gq:8180/auth/realms/badenair',
 };
